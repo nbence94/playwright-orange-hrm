b9374379708b6487cfd2bc5cfa6ec39b0f57bbf1
nbence94 <n.bence094@gmail.com>
2026-02-21 18:19:42 +0100

    add user fix

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  timeout: 30000,
+  timeout: 60000,
   reporter: [
     ['html', { outputFolder: './framework/data/reports' }],
     ['junit', { outputFile: './framework/data/reports/junit-report.xml' }],
